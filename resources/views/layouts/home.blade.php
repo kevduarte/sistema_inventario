8f9e0753e8d5544d4990ad5688321ac5aa07fc47
KEVIN <keving.gap@gmail.com>
2020-05-13 02:37:28 -0600

    First commit

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" href="{{asset('/image/ito.ico')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
    
-    <link  rel="stylesheet" href="{{asset('css/estilohome.css')}}" type="text/css">
+    <link  rel="stylesheet" href="{{asset('/css/estilohome.css')}}" type="text/css">
 
      <title>Inicio: @yield('titulo')</title>
   </head>
