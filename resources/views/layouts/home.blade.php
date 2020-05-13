9127ec24bdd7622cd2b025e1c124e1eaec342267
KEVIN <keving.gap@gmail.com>
2020-05-13 04:03:36 -0600

    First commit

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,56 +1,64 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Inicio: @yield('titulo')</title>
 
     <link rel="shortcut icon" href="{{asset('/image/ito.ico')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
 
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
    
     <link  rel="stylesheet" href="{{asset('/css/estilohome.css')}}">
 
-     <title>Inicio: @yield('titulo')</title>
   </head>
-
-
-
 
   <body style="background-image: url('image/logos_ito/itofondo.png'); background-size: cover; background-position:center; background-repeat: no-repeat; background-color: #FFFFFF  ;">
 
 
-    <nav class="navbar navbar-light" id="gob">
-      <a class="navbar-brand">
-        <img src="image/gobmx.png" width="85" height="20" ></a>
-      </nav>
+    <div class="container-fluid" id="gob">
+        <img src="image/gobmx.png" width="85" height="20" >
+    </div>
 
 <div class="container">
-  <div class="row justify-content-md-center">
-    <div class="col-md-3">
+  <div class="row">
+    <div class="col-md-3" align="center">
        <img  src="{{asset('image/tec.png')}}" width="250" height="140" alt=""/>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-6" align="center">
       <h1 id="ito1"><strong>Instituto Tecnológico de Oaxaca</strong></h1>
       <h2 id="dpto1"><strong>Departamento de Ciencias de la Tierra</strong></h2>
       <h3 id="lab1"><strong>Laboratorio de Ingeniería Civil</strong></h3>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3" align="center">
        <img src="{{asset('image/logoITO.png')}}" width="150" height="140" alt=""/>
     </div>
   </div>
 </div>
 
-<nav class="navbar navbar-dark" id="tecnm">
+<div class="container-fluid" id="tecnm">
+
+   <div class="row">
+    <div class="col-md-6" align="left">
+ <img src="image/ito.png" width="30" height="30" />
+   
+        </div>
+    
+    <div class="col-md-6" align="right">
+            <h4>ITO</h4>
+
+    </div>
+  </div>
   
-  <a class="navbar-brand" href="#">
-     <img src="image/ito.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    TecNM-ITO
-  </a>
-</nav>
+   
+  
+</div>
 
 
     @yield('seccion')
--- resources/views/layouts/plantilla_personal.blade.php
+++ resources/views/layouts/plantilla_personal.blade.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
 
   <title>Laboratorio de ingenieria civil @yield('title')</title>
   
